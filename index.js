de17d6ca15a9c0c42552a823febeaa27f03cc875
dimitrimitropulos <dimitrimitropoulos@gmail.com>
2017-01-11 15:02:27 -0500

    2.0.0.beta

--- index.js
+++ index.js
@@ -1,67 +1,341 @@
 module.exports = {
+  "parser": "babel-eslint",
   "parserOptions": {
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-      "jsx": true
-    }
+    "sourceType": "module",
+    "allowImportExportEverywhere": false
   },
-
   "plugins": [
-    "react", "import"
+    "immutable",
+    "flowtype",
+    "import",
+    "lodash",
+    "mocha",
+    "react"
   ],
-
+  "env": {
+    "browser": true,
+    "node": true,
+    "mocha": true,
+    "es6": true
+  },
   "extends": [
-    "standard-jsx"
+    "eslint:recommended",
+    "plugin:flowtype/recommended",
+    "plugin:lodash/recommended",
+    "plugin:react/recommended"
   ],
-
   "rules": {
-    // enforces no braces where they can be omitted
-    // http://eslint.org/docs/rules/arrow-body-style
-    'arrow-body-style': [2, 'as-needed'],
-    // require parens in arrow function arguments
-    'arrow-parens': 0,
-    // require space before/after arrow function's arrow
-    // http://eslint.org/docs/rules/arrow-spacing
-    'arrow-spacing': [2, { 'before': true, 'after': true }],
-    // Require a newline after the last import/require in a group
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
-    // TODO: enable
-    'import/newline-after-import': 0,
-    // disallow duplicate imports
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-    'import/no-duplicates': 2,
-    // enforce usage of spacing in template strings
-    // http://eslint.org/docs/rules/template-curly-spacing
-    'template-curly-spacing': 2,
-    // suggest using template literals instead of string concatenation
-    // http://eslint.org/docs/rules/prefer-template
-    'prefer-template': 2,
-    // use rest parameters instead of arguments
-    // http://eslint.org/docs/rules/prefer-rest-params
-    'prefer-rest-params': 2,
-    // suggest using of const declaration for variables that are never modified after declared
-    'prefer-const': 2,
-    /*
-    // suggest using arrow functions as callbacks
-    'prefer-arrow-callback': [2, {
-      'allowNamedFunctions': false,
-      'allowUnboundThis': true,
-    }],
-    */
+    // eslint *****************************************************************
+    // Possible Errors
+    "no-await-in-loop": "off",
+    "no-extra-parens": "off",
+    "no-prototype-builtins": "error",
+    "no-template-curly-in-string": "error",
+    "no-unsafe-negation": "error",
+    "valid-jsdoc": "off",
 
-    // require method and property shorthand syntax for object literals
-    // http://eslint.org/docs/rules/object-shorthand
-    'object-shorthand': [2, 'always'],
+    // Best Practices
+    "accessor-pairs": "error",
+    "array-callback-return": "error",
+    "block-scoped-var": "error",
+    "class-methods-use-this": "off",
+    "complexity": "off", // unfortunately, reducers screw this up
+    "consistent-return": "off",
+    "curly": "error",
+    "default-case": "error",
+    "dot-location": ["error", "property"],
+    "dot-notation": "off", // has trouble with promise.catch()
+    "eqeqeq": "error",
+    "guard-for-in": "error",
+    "no-alert": "error",
+    "no-caller": "warn",
+    "no-div-regex": "warn",
+    "no-else-return": "error",
+    "no-empty-function": "error",
+    "no-empty-pattern": "error",
+    "no-eq-null": "error",
+    "no-eval": "error",
+    "no-extend-native": "error",
+    "no-extra-bind": "error",
+    "no-extra-label": "error",
+    "no-floating-decimal": "error",
+    "no-global-assign": "error",
+    "no-implicit-coercion": "error",
+    "no-implicit-globals": "off",
+    "no-implied-eval": "error",
+    "no-invalid-this": "error",
+    "no-iterator": "error",
+    "no-labels": "error",
+    "no-lone-blocks": "error",
+    "no-loop-func": "error",
+    "no-magic-numbers": "off",
+    "no-multi-spaces": "off",
+    "no-multi-str": "error",
+    "no-new-func": "error",
+    "no-new-wrappers": "error",
+    "no-new": "error",
+    "no-octal-escape": "error",
+    "no-param-reassign": "error",
+    "no-proto": "error",
+    "no-restricted-properties": "off",
+    "no-return-assign": "error",
+    "no-return-await": "error",
+    "no-script-url": "error",
+    "no-self-assign": "error",
+    "no-sequences": "error",
+    "no-throw-literal": "error",
+    "no-unmodified-loop-condition": "warn",
+    "no-unused-expressions": "error",
+    "no-useless-call": "error",
+    "no-useless-concat": "error",
+    "no-useless-escape": "error",
+    "no-useless-return": "error",
+    "no-void": "error",
+    "no-warning-comments": "off",
+    "no-with": "error",
+    "radix": "error",
+    "require-await": "error",
+    "vars-on-top": "off",
+    "wrap-iife": "warn",
+    "yoda": "off",
 
-    // require let or const instead of var
-    'no-var': 2,
-    // disallow importing from the same path more than once
-    // http://eslint.org/docs/rules/no-duplicate-imports
-    'no-duplicate-imports': 2,
+    // Strict Mode
+    "strict": "off",
 
-    // require trailing commas in multiline object literals
-    'comma-dangle': [2, 'always-multiline'],
-    // disallow modifying variables that are declared using const
-    'no-const-assign': 2,
+    // Variables
+    "init-declarations": "off",
+    "no-catch-shadow": "off",
+    "no-label-var": "error",
+    "no-restricted-globals": "error",
+    "no-shadow-restricted-names": "error",
+    "no-shadow": "off",
+    "no-undef-init": "error",
+    "no-undefined": "off",
+    "no-use-before-define": "error",
+
+    // Node.js and CommonJS
+    "callback-return": "off",
+    "global-require": "warn",
+    "handle-callback-err": "off",
+    "no-mixed-requires": "error",
+    "no-new-require": "error",
+    "no-path-concat": "error",
+    "no-process-env": "off",
+    "no-process-exit": "off",
+    "no-restricted-modules": "off",
+    "no-sync": "warn",
+
+    // Stylistic Issues
+    "array-bracket-spacing": ["error", "never"],
+    "block-spacing": "error",
+    "brace-style": ["error", "1tbs", { "allowSingleLine": true }],
+    "camelcase": "off",
+    "capitalized-comments": "off",
+    "comma-dangle": ["error", "always-multiline"],
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "comma-style": "off",
+    "computed-property-spacing": ["error", "never"],
+    "consistent-this": "off",
+    "eol-last": "error",
+    "func-call-spacing": ["error", "never"],
+    "func-name-matching": "off",
+    "func-names": "off",
+    "func-style": "off",
+    "id-blacklist": "off",
+    "id-length": ["error", { "min": 2, "exceptions": ["i", "x", "y", "a", "b", "_"] }],
+    "id-match": "off",
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "jsx-quotes": "error",
+    "key-spacing": ["error", { "beforeColon": false, "mode": "minimum" }],
+    "keyword-spacing": "error",
+    "line-comment-position": "off",
+    "linebreak-style": "off",
+    "lines-around-comment": "off",
+    "lines-around-directive": "error",
+    "max-depth": "error",
+    "max-len": ["warn", { "code": 120 }],
+    "max-lines": ["warn", { "skipBlankLines": true, "max": 300}],
+    "max-nested-callbacks": ["warn", { "max": 4 }],
+    "max-params": ["warn", { "max": 5 }],
+    "max-statements-per-line": ["error", { "max": 3 }],
+    "max-statements": "off",
+    "multiline-ternary": "off",
+    "new-cap": "warn",
+    "new-parens": "error",
+    "newline-after-var": "off",
+    "newline-before-return": "off",
+    "newline-per-chained-call": "off",
+    "no-array-constructor": "error",
+    "no-bitwise": "off",
+    "no-continue": "off",
+    "no-inline-comments": "off",
+    "no-lonely-if": "error",
+    "no-mixed-operators": "error",
+    "no-multiple-empty-lines": "error",
+    "no-new-object": "error",
+    "no-nested-ternary": "error",
+    "no-plusplus": "warn",
+    "no-restricted-syntax": "off",
+    "no-tabs": "error",
+    "no-ternary": "off",
+    "no-trailing-spaces": "error",
+    "no-unneeded-ternary": "error",
+    "no-whitespace-before-property": "error",
+    "object-curly-newline": "off",
+    "object-curly-spacing": ["warn", "always", { "arraysInObjects": false }],
+    "object-property-newline": "off",
+    "one-var-declaration-per-line": "off",
+    "one-var": ["error", "never"],
+    "operator-assignment": "error",
+    "operator-linebreak": "off",
+    "padded-blocks": "off",
+    "quote-props": ["error", "consistent-as-needed"],
+    "quotes": ["error", "single"],
+    "require-jsdoc": "off",
+    "semi": "error",
+    "semi-spacing": "error",
+    "sort-keys": "warn",
+    "sort-vars": "off",
+    "space-before-blocks": "error",
+    "space-before-function-paren": ["error", "never"],
+    "space-in-parens": "off",
+    "space-infix-ops": "error",
+    "space-unary-ops": "error",
+    "spaced-comment": "off",
+    "unicode-bom": "error",
+    "wrap-regex": "error",
+
+    // ECMAScript 6
+    "arrow-body-style": ["error", "as-needed"],
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-spacing": ["error", { "before": true, "after": true }],
+    "generator-star-spacing": ["error", "before"],
+    "no-confusing-arrow": ["error", { "allowParens": true }],
+    "no-duplicate-imports": "error",
+    "no-restricted-imports": "off",
+    "no-useless-computed-key": "error",
+    "no-useless-constructor": "error",
+    "no-useless-rename": "error",
+    "no-var": "error",
+    "object-shorthand": ["error", "always"],
+    "prefer-arrow-callback": "off",
+    "prefer-const": "error",
+    "prefer-numeric-literals": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "prefer-template": "warn",
+    "rest-spread-spacing": "error",
+    "sort-imports": "off",
+    "symbol-description": "error",
+    "template-curly-spacing": "error",
+    "yield-star-spacing": "error",
+
+    // immutable ***************************************************************
+    "immutable/no-mutation": "warn",
+
+    // flowtype ****************************************************************
+    "flowtype/no-dupe-keys": "error",
+    "flowtype/no-primitive-constructor-types": "error",
+    "flowtype/object-type-delimiter": "error",
+    "flowtype/require-variable-type": "off",
+    "flowtype/sort-keys": "error",
+
+    // import ******************************************************************
+    // static analysis
+    "import/default": "error",
+    "import/named": "error",
+    "import/namespace": "error",
+    "import/no-absolute-path": "error",
+    "import/no-dynamic-require": "error",
+    "import/no-internal-modules": "off",
+    "import/no-restricted-paths": "error",
+    "import/no-unresolved": "off", // would be really nice if we could get this to work
+    "import/no-webpack-loader-syntax": "error",
+
+    // helpful warnings
+    "import/export": "error",
+    "import/no-deprecated": "off", // we don't use JSDoc
+    "import/no-extraneous-dependencies": "warn",
+    "import/no-mutable-exports": "error",
+    "import/no-named-as-default": "error",
+    "import/no-named-as-default-member": "error",
+
+    // module systems
+    "import/no-amd": "error",
+    "import/no-commonjs": "error",
+    "import/no-nodejs-modules": "error",
+    "import/unambiguous": "error",
+
+    // style guide
+    "import/extensions": "error",
+    "import/first": "error",
+    "import/max-dependencies": "warn",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "import/no-named-default": "error",
+    "import/no-namespace": "error",
+    "import/no-unassigned-import": "error",
+    "import/order": "off",
+    "import/prefer-default-export": "error",
+
+    // lodash ******************************************************************
+    "lodash/identity-shorthand": ["error", "always"],
+    "lodash/matches-prop-shorthand": ["error", "never"],
+    "lodash/matches-shorthand": ["error", "always"],
+    "lodash/prefer-lodash-method": "off",
+    "lodash/prefer-lodash-typecheck": "warn",
+    "lodash/prefer-noop": "off",
+    "lodash/preferred-alias": "off",
+
+    // react *******************************************************************
+    // JSX rules
+    "react/jsx-boolean-value": "error",
+    "react/jsx-closing-bracket-location": ["warn", "tag-aligned"],
+    "react/jsx-curly-spacing": ["warn", "never"],
+    "react/jsx-equals-spacing": "error",
+    "react/jsx-filename": "off",
+    "react/jsx-first-prop-new-line": ["error", "multiline"],
+    "react/jsx-handler-names": "off",
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-key": "error",
+    "react/jsx-max-props-per-line": ["error", { "maximum": 4 }],
+    "react/jsx-no-bind": ["warn", { "ignoreRefs": true }],
+    "react/jsx-no-comment-textnodes": "error",
+    "rezct/jsx-no-string-literals": "off",
+    "react/jsx-no-target-blank": "error",
+    "react/jsx-pascal-case": "warn",
+    "react/jsx-sort-props": "error",
+    "react/jsx-space-before-closing": ["error", "always"],
+    "react/jsx-tag-spacing": "error",
+    "react/jsx-wrap-multilines": "error",
+
+    // react rules
+    "react/forbid-component-props": "off",
+    "react/forbid-propTypes": "off", // FlowType handles this
+    "react/no-array-index-key": "error",
+    "react/no-children-prop": "error",
+    "react/no-danger": "warn",
+    "react/no-danger-with-children": "warn",
+    "react/no-did-mount-set-state": ["error", "disallow-in-func"],
+    "react/no-did-update-set-state": ["error", "disallow-in-func"],
+    "react/no-multi-comp": ["error", { "ignoreStateless": true }],
+    "react/no-set-state": "off",
+    "react/no-string-refs": "error",
+    "react/no-unescaped-entities": "off",
+    "react/no-unused-prop-types": "off", // FlowType handles this
+    "react/prefer-es6-class": "error",
+    "react/prefer-stateless-function": ["error", { "ignorePureComponents": true }],
+    "react/prop-types": "off", // FlowType handles this
+    "react/require-default-props": "warn",
+    "react/require-optimization": "warn",
+    "react/self-closing-comp": "error",
+    "react/sort-comp": ["error", { "order": [ "constructor", "static-methods", "lifecycle", "everything-else", "render" ]}],
+    "react/sort-prop-types": "off", // FlowType makes irrelevant
+    "react/style-prop-object": "error"
+  },
+  "globals": {
+    "bzTrack": false,
+    "bzLog": false
   }
 }
